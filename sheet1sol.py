774fa7062d81d39738a17fee4e6716393f646420
komalbhamu <99953334+komalbhamu@users.noreply.github.com>
2022-03-02 07:20:21 +0530

    Update sheet1sol.py

--- sheet1sol.py
+++ sheet1sol.py
@@ -73,6 +73,26 @@ n3=int("%s%s%s"%(n,n,n))
 total=n1+n2+n3
 print(total)
 
+#Q10
+
+l1=[]
+l2=[]
+x=int(input("Enter the no of elements\n"))
+y=int(input("Enter the no of elements\n"))
+print("Enter the first list elements\n")
+for i in range (0,x):
+   e = int(input())
+   l1.append(e)
+print("Enter the second list elements\n")
+
+for i in range (0,y):
+   t = int(input())
+   l2.append(t)
+
+print(l1)
+print(l2)
+
+
 #Q11
 
 z=input().split(',')
